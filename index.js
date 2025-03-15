20e0a1df6b58fd2f6add14e2bb4710a4696d0404
muhammed-jamiu <anatemuhammed@gmail.com>
2025-03-15 16:48:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Day-17-javascript-challenge");
+const skills = ["HTML", "CSS", "JS", "React"];
+//Skills array has to be stringified first to keep the format.
+
+const skillsJSON = JSON.stringify(skills, undefined, 4);
+localStorage.setItem("skills", skillsJSON);
+console.log(localStorage);
+
+///
+let skillss = [
+  { tech: "HTML", level: 10 },
+  { tech: "CSS", level: 9 },
+  { tech: "JS", level: 8 },
+  { tech: "React", level: 9 },
+  { tech: "Redux", level: 10 },
+  { tech: "Node", level: 8 },
+  { tech: "MongoDB", level: 8 },
+];
+let skillJSON = JSON.stringify(skillss);
+localStorage.setItem("skills", skillJSON);
